8aa42e2afdaf420490969040a6cc3ab5732b4f53
maustun1 <111627680+maustun1@users.noreply.github.com>
2023-11-20 18:33:06 +0300

    Add files via upload

---
+++ madlibsss.py
@@ -0,0 +1,13 @@
+youtuber = "Pewdiepie"
+print("Subscirbe to " + youtuber)
+print("Subscribe to {}".format(youtuber) )
+print(f"Subscribe to {youtuber}")
+adj = input("Random Adjective: ")
+food = input("Random Food: ")
+do = input("Random Verb: ")
+celebrity = input("Random famous person: ")
+
+
+madlib = f"Computer programming is so {adj} and it makes me feel so good \
+ I love eating {food}. Stay hydrated and {do} like you are {celebrity}. "
+print(madlib)
